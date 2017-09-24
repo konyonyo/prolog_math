bd2fe74ce6f0c366138249b5bd035d192458914a
konyonyo <yuichiro.utsumi@gmail.com>
2017-09-24 15:32:54 +0900

    なんか冗長だなぁ

--- sum.pl
+++ sum.pl
@@ -14,3 +14,22 @@ arith_sum(1, A, D, A).
 arith_sum(N, S, D, A) :- M is N - 1,
                          arith_sum(M, T, D, A),
                          S is T + D.
+
+recipro_sum(1, 1).
+recipro_sum(N, S) :- M is N - 1,
+                     recipro_sum(M, T),
+                     S is T + 1 / N.
+
+odd1(N) :- 1 is N mod 2.
+even1(N) :- 0 is N mod 2.
+
+odd_sqr_sum(1, 1).
+odd_sqr_sum(2, 1).
+odd_sqr_sum(I, S) :- odd1(I),
+                     J is I - 2,
+                     odd_sqr_sum(J, T),
+                     S is T + I ^ 2.
+odd_sqr_sum(I, S) :- even1(I),
+                     J is I - 1,
+                     odd_sqr_sum(J, S).
+
